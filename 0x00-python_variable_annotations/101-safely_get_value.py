44b4b69e3d1ecf4624cfc11493fec89597b88d9e
jaafar <jaafarferes@gmail.com>
2023-01-23 16:48:10 +0100

    use Mapping and TypeVar and Any

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,11 +2,12 @@
 """Given the parameters and the return values, add type annotations
 to the function In order to pass the checks, be carefull with the order
 of the Union. Hint: look into TypeVar """
-from typing import Sequence, TypeVar, Tuple, Iterable, Union, Any, Dict, Mapping
+from typing import Sequence, TypeVar, Tuple, Union, Any, Dict, Mapping
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)-> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """
     use Mapping and TypeVar and Any
     """
