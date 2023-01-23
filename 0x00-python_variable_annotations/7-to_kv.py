a95b2a4a89b50cab203f46fe253257dbf246c354
jaafar <jaafarferes@gmail.com>
2023-01-23 13:50:31 +0100

    pass the first element as str and multiply the others using tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""type-annotated function to_kv that takes a string k and an int OR float
+v as arguments and returns a tuple"""
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    """pass the first element as str and multiply the others"""
+    square: tuple[str, float] = (k, v**2)
+    return square
