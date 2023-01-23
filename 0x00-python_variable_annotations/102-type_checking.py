fd7baccca1c3f37d3b3b6c450175c9bf10392599
jaafar <jaafarferes@gmail.com>
2023-01-23 17:10:03 +0100

    test

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This file contains various functions with type hints.
+Functions perform different operations on input data and return result.
+All functions have been annotated with type hints.
+"""
+from typing import List, Tuple, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    This function takes a list and an optional integer factor as input and
+    returns a new list containing the elements of the original list repeated
+    factor number of times. """
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
