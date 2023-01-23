30f6d39bf44347c78b8e73d63e0ede5d0580e21d
jaafar <jaafarferes@gmail.com>
2023-01-23 13:29:40 +0100

    accept int and float and return the sum as float using Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -8,7 +8,3 @@ from typing import List, Union
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """accept int and float and return the sum as float"""
     return sum(mxd_lst)
-
-
-
-
