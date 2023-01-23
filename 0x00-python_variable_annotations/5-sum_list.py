34171af3d6ba952e010b7fe1a9060453061c84cb
jaafar <jaafarferes@gmail.com>
2023-01-23 12:42:13 +0100

    correction

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,6 +5,6 @@ returns their sum as a float. """
 
 
 
-def sum_list(input_list: list([float])) -> list([float]):
+def sum_list(input_list: list([float])) -> float:
     """ takes a list input_list of floats and return their sum"""
     return sum(input_list)
