85b6a71383c27beb39eab26bc9f854d6dfc8ff09
jaafar <jaafarferes@gmail.com>
2023-01-25 17:32:49 +0100

    1. Async Comprehensions  function

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-"""_summary_
+"""
+_summary_
 """
 import random
 import asyncio
-from typing import List, Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """_summary_
+    Async Comprehensions
+    Returns:
+        list[float]: random numbers
+    """
     numbers = []
     async for i in async_generator():
         numbers.append(i)
     return numbers
-
-
-
-
