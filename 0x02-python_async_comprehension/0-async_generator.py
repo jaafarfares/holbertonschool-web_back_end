f9ab9c83bc612abc1396c9cd61773da4e85573cb
jaafar <jaafarferes@gmail.com>
2023-01-25 16:41:12 +0100

    Async Generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@
 """
 import random
 import asyncio
-from typing import List, Generator
+from typing import List, AsyncGenerator
 
 
-async def async_generator() -> Generator[float, float, float]:
+async def async_generator() -> AsyncGenerator[float, float]:
     """
     use the async for loop to iterate over
     the values yielded by the coroutine
