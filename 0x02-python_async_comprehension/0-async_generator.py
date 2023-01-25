773b9068ef69c45ce116f602c9ef1c03a72e1c4c
jaafar <jaafarferes@gmail.com>
2023-01-25 16:10:26 +0100

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """_summary_
 """
-
 import random
 import asyncio
 
 
-
-
 async def async_generator():
+    """_summary_
+
+    Yields:
+        _type_: _description_
+    """
     for i in range(4):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
