3761e690b2997935aea258ea5447046422b48226
jaafar <jaafarferes@gmail.com>
2023-02-02 19:18:02 +0100

    0. Basic dictionary

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """
+BasicCache that inherits from BaseCaching and is a caching system
 """
 import requests
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """_summary_
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
     def __init__(self):
         super().__init__()
         self.cache_data = {}
