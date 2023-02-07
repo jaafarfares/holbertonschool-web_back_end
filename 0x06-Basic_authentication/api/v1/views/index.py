5551bacd8e1f9fb933afb94cfa7c6b562d1e97df
jaafar <jaafarferes@gmail.com>
2023-02-07 12:49:11 +0100

    fix

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -4,6 +4,7 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -20,6 +21,7 @@ def unauthorized() -> str:
         - the err 401 using abort
     """
     abort(401)
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
