e8a4b482d228eb57b3eb932cd9b7eb7cc4ce958e
jaafar <jaafarferes@gmail.com>
2023-02-10 15:39:02 +0100

    updates

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -17,7 +17,6 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """_summary_
-
         Args:
             user_id (str, optional): _description_. Defaults to None.
 
@@ -29,3 +28,15 @@ class SessionAuth(Auth):
         session_id = int(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """_summary_
+        Args:
+            session_id (str, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
