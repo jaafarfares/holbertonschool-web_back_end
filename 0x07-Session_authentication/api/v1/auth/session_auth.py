4ec8aa82fcbf5ed11a542efcacd9aa6d226dc5de
jaafar <jaafarferes@gmail.com>
2023-02-10 14:08:19 +0100

    finely

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -5,7 +5,12 @@ SessionAuth class
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
+    """1. Empty session
+    Args:
+        Auth (_class_): inherits from class Auth
+    """
     pass
