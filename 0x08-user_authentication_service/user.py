70efced446e6981c3243eb6d41802329eaa7ca55
jaafar <jaafarferes@gmail.com>
2023-02-16 14:39:44 +0100

    SQLAlchemy model named User

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -17,7 +17,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String)
-    hashed_password = Column(String)
-    session_id = Column(String)
-    reset_token = Column(String)
+    email = Column(String(250))
+    hashed_password = Column(String(250))
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
