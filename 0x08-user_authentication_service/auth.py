0f505f968b60b5a01644592ddf34f3f21bebfa6e
jaafar <jaafarferes@gmail.com>
2023-02-18 14:45:02 +0100

    updates

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,9 +3,32 @@
 _hash_password returns bytes hashed with bcrypt.hashpw
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.sql import exists
 
 
 def _hash_password(password: str) -> bytes:
     """function to hash the password"""
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+        return user
