7835766c6f6dcab009f0b66a55bec48062b2d6fa
jaafar <jaafarferes@gmail.com>
2023-02-18 15:36:20 +0100

    test

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -32,3 +32,15 @@ class Auth:
         except NoResultFound:
             user = self._db.add_user(email, _hash_password(password))
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        validate the user Credentials
+        """
+        try:
+            self._db.find_user_by(email=email)
+            hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            if bcrypt.checkpw(password.encode('utf-8'), hashed):
+                return True
+        except (ValueError, AttributeError,  NoResultFound):
+            return False
