d504cddba59c1a368bc5012da0cf2e4bd01dd242
jaafar <jaafarferes@gmail.com>
2023-02-18 14:06:02 +0100

    hash password function

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+_hash_password returns bytes hashed with bcrypt.hashpw
+"""
+import bcrypt
+
+
+def _hash_password(password):
+    if password:
+        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        return hashed
