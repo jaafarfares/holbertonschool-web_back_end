ede878035d12a97db77c15d5233e719acee517c2
jaafar <jaafarferes@gmail.com>
2023-02-18 15:45:55 +0100

    updates

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -15,7 +15,7 @@ def _hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
 
-def _generate_uuid (self) -> str:
+def _generate_uuid () -> str:
         """_summary_
 
         Returns:
