d121c3588fee5c3c5d510de968595c341b53fcb1
jaafar <jaafarferes@gmail.com>
2023-02-18 15:14:33 +0100

    updates

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -23,7 +23,7 @@ def users() -> str:
     email = request.form.get("email")
     password = request.form.get("password")
     try:
-        AUTH.register(email, password)
+        AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
