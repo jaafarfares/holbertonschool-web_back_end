f995d9c811b616c0f204aa329958cb28ae3e97cd
jaafar <jaafarferes@gmail.com>
2023-02-18 14:08:07 +0100

    check*

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -5,7 +5,7 @@ _hash_password returns bytes hashed with bcrypt.hashpw
 import bcrypt
 
 
-def _hash_password(password):
+def _hash_password(password: str) -> bytes:
     """function to hash the password"""
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
