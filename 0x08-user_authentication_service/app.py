32ee3a7b343b6e66b5c815a2bb784d2d94068ffd
jaafar <jaafarferes@gmail.com>
2023-02-19 19:16:46 +0100

    updates

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -28,6 +28,18 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """
+    register user app
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if AUTH.valid_login(email, password):
+        return AUTH.create_session(email)
+    abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
