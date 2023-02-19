aae210437d595943f80f1116d67f63fc805ca60b
jaafar <jaafarferes@gmail.com>
2023-02-19 19:22:42 +0100

    updates

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -38,7 +38,6 @@ def login() -> str:
 
     if AUTH.valid_login(email, password):
         response = jsonify({"email": "<user email>", "message": "logged in"})
-        response.set_cookie('session_id', AUTH.create_session(email))
         return response.set_cookie('session_id', AUTH.create_session(email))
     abort(401)
 
