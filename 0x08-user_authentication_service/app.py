2b24e810ff92a5c067a2921bfff248f61090df7d
jaafar <jaafarferes@gmail.com>
2023-02-19 19:38:22 +0100

    updates

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -39,9 +39,10 @@ def login() -> str:
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
         response = jsonify({"email": email, "message": "logged in"})
-        response.set_cookie('session_id', session_id)
+        response.set_cookie('session_id', session_id, secure=True, httponly=True)
         return response
     abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
