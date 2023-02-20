7df1250a80dcee78066e147f95d284f4840c8ad6
jaafar <jaafarferes@gmail.com>
2023-02-20 16:37:45 +0100

    updates

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+first unittest
+"""
+import unittest
+from unittest.mock import patch
+from utils import *
+from parameterized import parameterized
+from collections.abc import Mapping
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """_summary_
+    Args:
+        unittest (TestCase): test_access_nested_map
+    """
+    @parameterized.expand([
+        ({'a': 1}, ['a'], 1),
+        ({'a': {'b': 2}}, ['a'], {'b': 2}),
+        ({'a': {'b': 2}}, ['a', 'b'], 2),
+    ])
+    def test_access_nested_map(self, i, a, b):
+        """
+        Parameterize a unit test
+        """
+        self.assertEqual(access_nested_map(i, a), b)
+
+    
+    @parameterized.expand([({}, ['a'], KeyError), ({'a': 1}, ['a', 'b'], KeyError)])
+    def test_access_nested_map_exception(self, i, a, ex):
+        self.assertRaises(ex)
+
+if __name__ == '__main__':
+    unittest.main()
