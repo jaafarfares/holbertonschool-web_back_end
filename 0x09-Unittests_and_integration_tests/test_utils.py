1de438dc59f7d23423637bb54309264542398c0b
jaafar <jaafarferes@gmail.com>
2023-02-21 10:35:43 +0100

    Mock HTTP calls with patch

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -3,10 +3,14 @@
 first unittest
 """
 import unittest
-from unittest.mock import patch
-from utils import *
+from unittest.mock import patch, MagicMock
+from utils import get_json, access_nested_map
 from parameterized import parameterized
 from collections.abc import Mapping
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,13 +29,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(i, a), b)
 
-    
-    @parameterized.expand([({}, ['a'], KeyError), ({'a': 1}, ['a', 'b'], KeyError)])
+    @parameterized.expand([({}, ['a'], KeyError),
+                          ({'a': 1}, ['a', 'b'], KeyError)])
     def test_access_nested_map_exception(self, i, a, ex):
         """_summary_
         Parameterize a unit test
         """
         self.assertRaises(ex)
 
-if __name__ == '__main__':
-    unittest.main()
+
+class TestGetJson(unittest.TestCase):
+    """
+    get json test class
+    """
+    @patch('utils.requests')
+    def test_get_json(self, mock_requests):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {{'test_url="http://example.com"',
+                                            'test_payload={"payload": True}'},
+                                           {'test_url="http://holberton.io"',
+                                            'test_payload={"payload": False}'}}
+        mock_requests.get.return_value = mock_response
+        self.assertEqual(get_json(), ('False', 'True'))
