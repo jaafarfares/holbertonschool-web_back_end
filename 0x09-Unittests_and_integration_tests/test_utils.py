0a5fc2c3caf64ce50dc16c2d01f0fb655a88d919
jaafar <jaafarferes@gmail.com>
2023-02-21 14:43:25 +0100

    updates

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 first unittest
 """
 import unittest
+from unittest import mock
 from unittest.mock import patch, MagicMock, Mock
 from utils import *
 from parameterized import parameterized
@@ -43,10 +44,10 @@ class TestGetJson(unittest.TestCase):
                            ('test_url=http://holberton.io',
                             {"payload": False})])
     def test_get_json(self, url, payload):
-        mock_response = MagicMock()
+        mock_response = Mock()
         mock_response.status_code = 200
         mock_response.json.return_value = payload
-        with patch('requests.get', return_value=mock_response):
+        with mock.patch('requests.get', return_value=mock_response):
             self.assertEqual(get_json(url), payload)
             mock_response.json.assert_called_once()
 
