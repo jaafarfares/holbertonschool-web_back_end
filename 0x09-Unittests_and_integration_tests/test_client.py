9156b586b374e2f2a3ef440a144bcd45f8e49ed5
jaafar <jaafarferes@gmail.com>
2023-02-22 22:24:32 +0100

    updateq

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -38,16 +38,15 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, mocked_method):
         """self descriptive"""
-        mock_payload = [{"name": "Microsoft"}, {"name": "Apple"}]
+        mock_payload = [{"name": "Microsoft"}, {"name": "name"}]
         mocked_method.return_value = mock_payload
 
         with patch('client.GithubOrgClient._public_repos_url',
                    new_callable=PropertyMock) as mocked_public:
-            mocked_public.return_value = {"name", "Microsoft"}
+            mocked_public.return_value = ["Microsoft", "name"]
             response = GithubOrgClient('org')
-            a = response._public_repos_url
-            self.assertEqual(a, mocked_public.return_value)
-            mocked_method.get_json()
+            self.assertEqual(response.public_repos(), mocked_public.return_value)
+            mocked_method.assert_called_once()
             mocked_public.assert_called_once()
 
 
