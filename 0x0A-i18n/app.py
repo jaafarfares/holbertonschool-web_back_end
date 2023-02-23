f29bdc64a15648d64f602f095233cc42f9cec7b6
jaafar <jaafarferes@gmail.com>
2023-02-23 16:38:33 +0100

    upates

---
+++ 0x0A-i18n/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+basic flask app1
+"""
+from flask import Flask, render_template
+from datetime import datetime
+app = Flask(__name__, template_folder='templates')
+
+
+
+@app.route('/', methods=["GET"])
+def index():
+    """
+    index function
+    """
+    current_time = datetime.now().strftime("%b %d, %Y, %I:%M:%S %p")
+    return render_template('index.html', current_time=current_time)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, debug=True)
