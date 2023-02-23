59b6067ca53bd30e524d4d19782c417c4191cca2
jaafar <jaafarferes@gmail.com>
2023-02-23 16:42:20 +0100

    upates

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -1,42 +1,53 @@
 #!/usr/bin/env python3
 """
-basic flask app1
+Basic Flask App 4
 """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 import locale
+
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
-
 class Config:
-    """class cofig for the Flask app"""
+    """
+    Class config for Flask app
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
 app.config.from_object(Config)
-
 
 @babel.localeselector
 def get_locale():
     """
-    get_locale function
+    Get the best matching language based on accept headers and URL param locale
     """
     user = getattr(g, 'user', None)
     if user is not None:
         return user.locale
+
+    # Check if URL parameter locale is supported and return it
+    if request.args.get('locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+@app.before_request
+def before_request():
+    """
+    Set the user's locale based on URL param locale
+    """
+    if request.args.get('locale') in app.config['LANGUAGES']:
+        g.user = type('User', (object,), {'locale': request.args.get('locale')})
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=['GET'])
 def index():
     """
-    index function
+    Index function
     """
-    return render_template('3-index.html')
-
+    return render_template('4-index.html')
 
 if __name__ == '__main__':
     app.run(use_reloader=True, debug=True)
