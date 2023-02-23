9c71f179a1a67c6996cfe159bb3ca1437f86a3ae
jaafar <jaafarferes@gmail.com>
2023-02-23 15:24:48 +0100

    updates

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+basic flask app1
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config:
+    """class cofig for the Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """
+    index function
+    """
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get_locale function
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, debug=True)
