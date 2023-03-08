fdd36d0947e6a93cfd5e8d186384dc24bc2ae1a2
jaafar <jaafarferes@gmail.com>
2023-03-08 12:24:50 +0100

    up

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- create a simple table users
 CREATE TABLE IF NOT EXISTS users (
-    id int PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email varchar(255) UNIQUE NOT NULL,
     name varchar(255)
 );
