c2472dfac9a3b7615be220193457ec35991487ae
jaafar <jaafarferes@gmail.com>
2023-03-09 13:46:18 +0100

    check

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email varchar(255) UNIQUE NOT NULL,
     name varchar(255),
-    country ENUM('US', 'CO', 'TN')
+    country ENUM('US', 'CO', 'TN') NOT NULL
 );
