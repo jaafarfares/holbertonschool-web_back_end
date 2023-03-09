e72943b858cab45597334e3a6e5b9dd402b82af5
jaafar <jaafarferes@gmail.com>
2023-03-09 13:47:51 +0100

    updates

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,4 @@
 -- script that ranks county's origins of bands
 SELECT origin, SUM(num_members) AS nb_fans
-FROM metal_bands
-GROUP BY origin
+FROM metal_bands GROUP BY origin
 ORDER BY nb_fans DESC;
