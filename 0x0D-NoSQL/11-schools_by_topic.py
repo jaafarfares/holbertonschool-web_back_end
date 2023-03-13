afc7d79429c8fb2766b6e2b88fe104f168732a7d
jaafar <jaafarferes@gmail.com>
2023-03-13 16:15:26 +0100

    Python function that returns the list of school having a specific topic

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Python function that returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """schools_by_topic"""
+    if mongo_collection is None:
+        return None
+    return mongo_collection.find({"topics": topic})
