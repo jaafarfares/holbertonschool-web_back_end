d2c550ad596e060e9efbbf4a52d24ce0bb91eb93
jaafar <jaafarferes@gmail.com>
2023-03-13 15:36:31 +0100

    Python function that inserts a new document in a collection based on kwargs

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -5,6 +5,7 @@ Python function that inserts a new document in a collection based on kwargs
 
 
 def insert_school(mongo_collection, **kwargs):
+    """insert_school"""
     if mongo_collection is None:
         return None
     return mongo_collection.insert_one(kwargs).inserted_id
