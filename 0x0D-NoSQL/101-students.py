15f667e04248b8371bc444a7bb9d8c497d5c4673
jaafar <jaafarferes@gmail.com>
2023-03-13 16:33:54 +0100

    Python function that returns all students sorted by average score.

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python function that returns all students sorted by average score.
+"""
+
+
+def top_students(mongo_collection):
+    """top_students"""
+    top = [
+        {"$unwind": "$topics"},
+        {
+            "$group": {
+                "_id": "$_id",
+                "name": {"$first": "$name"},
+                "scores": {"$push": "$topics.score"}
+            }
+        },
+        {
+            "$project": {
+                "_id": 1,
+                "name": 1,
+                "averageScore": {"$avg": "$scores"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ]
+    return list(mongo_collection.aggregate(top))
