9dce5e93c29f3e8b27ddaa938ab73d9117a79b43
jaafar <jaafarferes@gmail.com>
2023-03-13 15:38:35 +0100

    Python function that inserts a new document in a collection based on kwargs

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" mongodb
+Python function that inserts a new document in a collection based on kwargs
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update_topics
+    """
+    if mongo_collection is None:
+        return None
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+
+
