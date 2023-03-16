acd73ba74ad426e3f41509f84c744e54656a148e
jaafar <jaafarferes@gmail.com>
2023-03-16 17:23:14 +0100

    Let's create a report object

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,15 +1,16 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const [department, employees] of Object.entries(employeesList)) {
-    allEmployees[department] = [...employees];
-  }
-
   return {
-    allEmployees,
-
-    getNumberOfDepartments(employees) {
-      return Object.keys(employees).length;
+    allEmployees: Object.entries(employeesList).reduce(
+      (acc, [department, employees]) => {
+        if (employees.length > 0) {
+          acc[department] = [...employees];
+        }
+        return acc;
+      },
+      {}
+    ),
+    getNumberOfDepartments: function () {
+      return Object.keys(this.allEmployees).length;
     },
   };
 }
