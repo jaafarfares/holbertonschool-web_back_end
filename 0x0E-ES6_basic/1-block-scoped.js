9ba2b8b620e033e397f5305fed140d08664789cb
jaafar <jaafarferes@gmail.com>
2023-03-16 09:30:33 +0100

    Block Scope

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    const task = true;
+    const task2 = false;
   }
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   return [task, task2];
 }
