0a21134d8cf7ab1996d3a184583f0c505ecc2317
jaafar <jaafarferes@gmail.com>
2023-03-16 10:34:19 +0100

    s p r e a d syntax

--- 0x0E-ES6_basic/5-spread-operator.js
+++ 0x0E-ES6_basic/5-spread-operator.js
@@ -1,1 +1,3 @@
-export default (array1, array2, string) => [...array1, ...array2, ...string]
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
+}
