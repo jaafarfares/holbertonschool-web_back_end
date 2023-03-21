12be820fa030dd192edeab58b2bea103a3dc4365
jaafar <jaafarferes@gmail.com>
2023-03-21 23:15:20 +0100

    .

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,11 +1,14 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const photo = results[0].body;
-      const { first, last } = results[1];
-      console.log(`${photo} ${first} ${last}`);
+      const [photo, user] = results;
+      console.log(`${user.body.firstName} ${user.body.lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
+
+export default handleProfileSignup;
