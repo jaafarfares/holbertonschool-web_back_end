727bd86c6e20b31a2563ee04cfb37c5bf2ff9f55
jaafar <jaafarferes@gmail.com>
2023-03-21 23:14:06 +0100

    return promise resolve or reject

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])    
-.then((results) => {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
       const photo = results[0].body;
       const { first, last } = results[1];
       console.log(`${photo} ${first} ${last}`);
