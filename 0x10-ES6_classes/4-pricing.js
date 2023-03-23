03e9b1dd51b63323c432cafe34e4afde8e0f2114
jaafar <jaafarferes@gmail.com>
2023-03-23 17:31:56 +0100

    return the amount multiplied by the conversion rate.

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof (amount) !== 'number') throw TypeError('must be a number');
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set name(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static  convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
