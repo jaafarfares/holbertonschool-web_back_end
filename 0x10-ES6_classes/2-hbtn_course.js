bfc8cbeda64c324ef337db695111de3ebed27b52
jaafar <jaafarferes@gmail.com>
2023-03-23 16:44:51 +0100

    Implement a getter and setter for each attribute.

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -13,24 +13,29 @@ export default class HolbertonCourse {
     get name() {
       return this._name;
     }
+
     set name(value) {
       if (typeof (value) !== 'string') {
         throw TypeError('Name must be a string');
       }
       this._name = value;
     }
+
     get length() {
       return this._length;
     }
+
     set length(value) {
       if (typeof (value) !== 'number') {
         throw TypeError('Length must be a number');
       }
       this._length = value;
     }
+
     get students() {
       return this._students;
     }
+
     set students(value) {
       if (students.every((i) => (typeof i !== 'string'))) {
         throw TypeError('Students must be an array');
