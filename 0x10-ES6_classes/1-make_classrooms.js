23e677c4d0d61be34c675719a479844b0f69ee05
jaafar <jaafarferes@gmail.com>
2023-03-23 14:36:17 +0100

    return an array of 3 ClassRoom objects with the sizes 19, 20, and 34

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
-export default initializeRooms => [
+export default function initializeRooms() {
+  return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)];
+    new ClassRoom(34),
+  ];
+}
