cc4eef7ef11e9ce905378708a2db4567ae5c543e
jaafar <jaafarferes@gmail.com>
2023-03-28 14:19:10 +0100

    returns an array of objects who are located in a specific city.  with filter()

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
 export default function getStudentsByLocation(arr, city) {
-  return arr.filter((student) => {
-    return student.location === city;
-  });
+  return arr.filter((student) => student.location === city);
 }
