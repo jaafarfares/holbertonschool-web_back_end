95127e3bce9c1f8e0ef5afdccfe8ba3c48325949
jaafar <jaafarferes@gmail.com>
2023-03-29 11:49:03 +0100

    test

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,15 @@
 const MAX = 5;
 
-export const  weakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     const count = weakMap.get(endpoint);
-    weakMap.set(endpoint, count +1);
+    weakMap.set(endpoint, count + 1);
     if (count >= MAX) {
       throw new Error('Endpoint load is high');
     }
   } else {
-  weakMap.set(endpoint, 1);
-
+    weakMap.set(endpoint, 1);
   }
 }
