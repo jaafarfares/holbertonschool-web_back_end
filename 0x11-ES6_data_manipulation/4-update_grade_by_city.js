dc30cab319dca3fbac8a7dd3f4d6014b7d7b625a
jaafar <jaafarferes@gmail.com>
2023-03-29 11:29:20 +0100

    returns an array of students for a specific city with their new grade

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,21 +1,7 @@
-export default function updateStudentGradeByCity(arr, city, grade) {
-    const updatedStudents = arr.map((student) => {
-        if (student.location === city) {
-          const matchingGrade = grade.find((grade) => grade.studentId === student.id);
-          if (matchingGrade) {
-            return {
-              ...student,
-              grade: matchingGrade.grade,
-            };
-          } else {
-            return {
-              ...student,
-              grade: 'N/A',
-            };
-          }
-        } else {
-          return student;
-        }
-      });
-      return updatedStudents;
-    }
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+    return arr.filter((student) => student.location === city).map((student) => {
+      let matchinggrades = newGrades.find((grade) => grade.studentId === student.id);
+      let grade = matchinggrades ? matchinggrades.grade : 'N/A';
+      return { ...student, grade };
+    });
+  }
