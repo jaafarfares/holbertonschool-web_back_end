813ed22ebe1e6f8c5eec9f428ec5a3799dfc5498
jaafar <jaafarferes@gmail.com>
2023-04-03 09:56:57 +0100

    test

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name? ', (answer) => {
+  console.log(`Your name is: ${answer}`);
+  console.log('This important software is now closing');
+});
+
+
+rl.on('close', () => {
+  process.exit(0);
+});
