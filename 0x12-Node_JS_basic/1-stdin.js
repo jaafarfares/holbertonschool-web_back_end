224497dc8a2897d742768eea9af95c2d6b1b4659
jaafar <jaafarferes@gmail.com>
2023-04-03 11:12:27 +0100

    Using Process stdin stdout

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+  const name = process.stdin.read();
 
-  if (chunk !== null) {
-    const name = chunk.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
+  if ((name) !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
-
-process.on('exit', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
