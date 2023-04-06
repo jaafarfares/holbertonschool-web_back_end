7d5c86881675de1aa045cf98b058eafd1df9cfe8
jaafar <jaafarferes@gmail.com>
2023-04-06 10:53:00 +0100

    simple mocha testt with assert

---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+
+
+describe('basicTest', function () {
+  it('should equal 3 when 1 + 2', function () {
+    var result = 1 + 2;
+    assert.equal(result, 3);
+  });
+});
