9ca398ba401eee4dc4e79a4748cf8bacc7939e70
jaafar <jaafarferes@gmail.com>
2023-04-06 11:03:45 +0100

    simple mocha testt with assert

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -6,5 +6,9 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   it('should equal 3 when 1 + 2', function () {
     assert.equal(calculateNumber(1, 2), 3);
+    assert.equal(calculateNumber(3, 2), 5);
+    assert.equal(calculateNumber(5, -2), 3);
+    assert.equal(calculateNumber(1, -1), 0);
+
   });
 });
