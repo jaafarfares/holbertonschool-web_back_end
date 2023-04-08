e479d422a4ea1d71fb17ff996b5bdc5d26e2096c
jaafar <jaafarferes@gmail.com>
2023-04-08 11:06:19 +0100

    add a new endpoint

--- 0x13-unittests_in_js/9-api/api.js
+++ 0x13-unittests_in_js/9-api/api.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the payment system')
   });
 
-  app.get('/cart/:id', (req, res) => {
+  app.get('/cart/:id([0-9]+)', (req, res) => {
     const cartId = req.params.id;
     res.send(`Payment methods for cart :${cartId}`)
   });
