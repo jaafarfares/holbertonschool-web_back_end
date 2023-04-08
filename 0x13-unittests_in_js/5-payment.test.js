9efbc9f5c5d763d60d03dab192cdb6eff28af13f
jaafar <jaafarferes@gmail.com>
2023-04-08 10:24:11 +0100

    test

---
+++ 0x13-unittests_in_js/5-payment.test.js
@@ -0,0 +1,39 @@
+const mocha = require('mocha');
+const { expect, assert } = require('chai');
+const sinon = require('sinon');
+
+const utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { spy } = require('sinon');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call calculateNumber', () => {
+    const stub = sinon.stub(utils, 'sendPaymentRequestToApi');
+    stub.returns(10);
+    const spyy = sinon.spy(console, 'log');
+    const req = sendPaymentRequestToApi(100, 20);
+
+    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
+    expect(spyy.calledOnceWithExactly('The total is: 120'));
+    expect(utils.calculateNumber('SUM', 100, 20)).to.equal(req);
+
+    stub.restore();
+    spyy.restore();
+  });
+  it('should call calculateNumber', () => {
+    const stub = sinon.stub(utils, 'sendPaymentRequestToApi');
+    stub.returns(10);
+    const spy = sinon.spy(console, 'log');
+    const req = sendPaymentRequestToApi(100, 20);
+
+    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
+    expect(spy.calledOnceWithExactly('The total is: 20'));
+    expect(utils.calculateNumber('SUM', 100, 20)).to.equal(req);
+
+    stub.restore();
+    spy.restore();
+  });
+
+
+
+});
