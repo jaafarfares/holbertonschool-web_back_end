97422b37a8b20eefe496ebd8df6f6dc6d2d02f54
jaafar <jaafarferes@gmail.com>
2023-09-18 12:52:12 +0100

    create a small HTTP server using the http module:

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -6,34 +6,30 @@ const countStudents = require('./3-read_file_async');
 const DATABASE = args[0];
 
 const hostname = 'localhost';
-const port = 1247; 
+const port = 1245;
 
 const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
   const { url } = req;
 
   if (url === '/') {
-    res.statusCode = 200;
-    res.end('Hello Holberton School!');
+    res.write('Hello Holberton School!');
   } else if (url === '/students') {
+    res.write('This is the list of our students\n');
     try {
       const students = await countStudents(DATABASE);
-      res.statusCode = 200;
-      res.write('This is the list of our students\n');
       res.end(`${students.join('\n')}`);
     } catch (error) {
-      res.statusCode = 500;
-      res.end('Cannot load the database');
+      res.end(error.message);
     }
-  } else {
-    res.statusCode = 404;
-    res.end();
   }
+  res.statusCode = 404;
+  res.end();
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
