34fdf5a0405ac3322518fb37de85a224368c8792
jaafar <jaafarferes@gmail.com>
2023-09-18 12:33:26 +0100

    create a small HTTP server using the http module:

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,13 +1,16 @@
+// Description: Create a simple HTTP server using Node's HTTP module.
 const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+app.listen(port, hostname, () => {
 });
 
 module.exports = app;
