84281e55ddf9d51379d98f52bae773631125e4a0
jaafar <jaafarferes@gmail.com>
2023-09-18 13:30:38 +0100

    create a small HTTP server using the http module:

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+const database = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.send('This is the list of our students\n');
+  try {
+    const students = await countStudents(database);
+    res.send(students.join('\n'));
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
